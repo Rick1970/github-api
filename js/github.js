95e63adb1887f557fe229179737a2fb3746fb4ab
Rick1970 <rthornbrug@gmail.com>
2016-08-19 15:07:32 -0700

    reach the repo level with api.

--- js/github.js
+++ js/github.js
@@ -1,20 +1,21 @@
 var apiKey = require('./../.env').apiKey;
 
 function Repo() {
+
 }
 
 Repo.prototype.getRepos = function(name) {
   console.log("I am a repo object");
-  console.log(name)
-  $.get('https://api.github.com/users/' + name +'?access_token=' + apiKey).then(function(response){
-    console.log(JSON.stringify(response.repos_url));
+  console.log(name);
+  $.get('https://api.github.com/users/' + name +'/repos?/access_token=' + apiKey).then(function(response){
+    console.log(JSON.stringify(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
 
 
-// exports.getRepos = function(){
+
 
 
 exports.repoModule = Repo;
