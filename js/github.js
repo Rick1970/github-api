d80ecf4fe6cd21018f3640bd50b6e50c76df4778
Rick1970 <rthornbrug@gmail.com>
2016-08-19 23:37:44 -0700

    work on styling.

--- js/github.js
+++ js/github.js
@@ -9,26 +9,19 @@ Repo.prototype.getRepos = function(name) {
   $.get('https://api.github.com/users/' + name +'/repos?/access_token=' + apiKey).then(function(response){
     console.log(JSON.stringify(response));
 
-
-
     for(var i = 0; i <= response.length; i++){
-
-      $('ul#final').append("<li>" + response[i].name + "   "  + response[i].description + "</li>");
+      $('ul#repos').append("<li>" + response[i].name + "   "  + response[i].description + "</li>");
+      if(response[i].description === null || response[i].description === ""){
+        $('ul#description').append("<li>" + "Sorry, no description provided" + "</li>");
+      } else {
+        $('ul#description').append("<li>" + response[i].description + "</li>");
+      }
     }
 
-    }).fail(function(error){
+  }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
-
-Repo.prototype.loadRepos = function(response) {
-
-
-
-};
-
-
-
 
 
 exports.repoModule = Repo;
